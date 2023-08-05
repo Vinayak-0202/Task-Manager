c9fa29fdd57f5c5c6aa479f78c4e2445c8a60c07
Vinayak-0202 <vpnikam02@gmail.com>
2023-08-06 01:07:30 +0530

    Compeletly implimented REST api and Crud operations

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,22 +1,67 @@
 const Task = require("../model/Task.js");
 
-exports.getAllTask = (req, res) => {
-  res.send("here is all your tasks");
+exports.getAllTask = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(201).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 };
 
-exports.gerSingleTask = (req, res) => {
-  res.send("here is single tasks");
+exports.gerSingleTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `Nop task with id ${req.params.id}` });
+    }
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(404).json({ err });
+  }
 };
 
 exports.createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 };
 
-exports.updateTask = (req, res) => {
-  res.json({ id: req.params.id });
+exports.updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `Nop task with id ${req.params.id}` });
+    }
+
+    res.status(200).json({ message: `your task updated sucessfuly` });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 };
 
-exports.deleteTask = (req, res) => {
-  res.json({ id: req.params.id });
+exports.deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `no task with id ${taskID}` });
+    }
+    // res.status(200).json({ message: `${task} is deleted sucessfuly` });
+    res.status(200).json({ task: null, statud: "sucess" });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 };
